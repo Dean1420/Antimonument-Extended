f2cdef837c97a1b697862dc05c6979486f48c5bb
Dean <dnbr1420@gmail.com>
2026-02-25 19:29:58 +0100

    Sliceable Objects now can be reset to pre cut state

--- Antimonument-Extended/Assets/!_Project/Systems/ObjectSlicer/Scripts/SliceObject.cs
+++ Antimonument-Extended/Assets/!_Project/Systems/ObjectSlicer/Scripts/SliceObject.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using EzySlice;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
 public class SliceObject : MonoBehaviour
 {
@@ -14,12 +15,39 @@ public class SliceObject : MonoBehaviour
     [SerializeField] private Material crossSectionMaterial;
     [SerializeField] public float cutForce = 2000;
 
+    // Lists to track objects
+    private List<GameObject> originalObjects = new List<GameObject>();
+    private List<GameObject> slicedObjects = new List<GameObject>();
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        CollectSliceableObjects();
+    }
+
+
+
+    private void CollectSliceableObjects()
+    {
+        // Find all objects on the sliceable layer at start
+        GameObject[] allObjects = FindObjectsOfType<GameObject>();
+        foreach (GameObject obj in allObjects)
+        {
+            // Check if object is on the sliceable layer
+            if (IsInLayerMask(obj, sliceableLayer))
+            {
+                originalObjects.Add(obj);
+            }
+        }
+    }
+
+
+
+    private bool IsInLayerMask(GameObject obj, LayerMask layerMask)
+    {
+        return layerMask == (layerMask | (1 << obj.layer));
     }
 
 
@@ -49,13 +77,34 @@ public class SliceObject : MonoBehaviour
     {
         GameObject upperHull = hull.CreateUpperHull(target, crossSectionMaterial);
         SetupSlicedComponent(upperHull, target.layer);
+        slicedObjects.Add(upperHull);
         
         GameObject lowerHull = hull.CreateLowerHull(target, crossSectionMaterial);
         SetupSlicedComponent(lowerHull, target.layer);
+        slicedObjects.Add(lowerHull);
         
-        Destroy(target);
+        HandleObjectCleanup(target);
     }
 }
+
+
+
+    private void HandleObjectCleanup(GameObject target)
+    {
+        // If it's an original object, disable it instead of destroying
+            if (originalObjects.Contains(target))
+            {
+                target.SetActive(false);
+            }
+            else if (slicedObjects.Contains(target))
+            {
+                // If it's a sliced object being re-sliced, remove from list and destroy
+                slicedObjects.Remove(target);
+                Destroy(target);
+            }
+    }
+
+
 
 public void SetupSlicedComponent(GameObject slicedObject, int layer)
 {
@@ -66,4 +115,28 @@ public void SetupSlicedComponent(GameObject slicedObject, int layer)
     collider.convex = true;
     rb.AddExplosionForce(cutForce, slicedObject.transform.position, 1);
 }
+
+
+
+ public void ResetCuts()
+    {
+        // Destroy all sliced objects
+        foreach (GameObject slicedObj in slicedObjects)
+        {
+            if (slicedObj != null)
+            {
+                Destroy(slicedObj);
+            }
+        }
+        slicedObjects.Clear();
+
+        // Re-enable all original objects
+        foreach (GameObject originalObj in originalObjects)
+        {
+            if (originalObj != null)
+            {
+                originalObj.SetActive(true);
+            }
+        }
+    }
 }
